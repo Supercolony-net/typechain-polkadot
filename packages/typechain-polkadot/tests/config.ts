872fb06bac14b1569ec3015490a4cf96666ba003
varex83 <limposfeed@gmail.com>
2022-07-20 15:07:37 +0300

    prepare-to-publish

--- packages/typechain-polkadot/tests/config.ts
+++ packages/typechain-polkadot/tests/config.ts
@@ -2,6 +2,7 @@ import * as PolkadotAPI from '@polkadot/api';
 import type {KeyringPair} from "@polkadot/keyring/types";
 
 const ecdsa_keyring = new PolkadotAPI.Keyring({ type:'ecdsa' });
+
 const UserAlice: KeyringPair = ecdsa_keyring.addFromUri('//Alice');
 const UserBob: KeyringPair = ecdsa_keyring.addFromUri('//Bob');
 const UserCharlie: KeyringPair = ecdsa_keyring.addFromUri('//Charlie');
