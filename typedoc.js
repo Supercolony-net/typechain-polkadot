0c79d09bb60921d31dc184753281d1f91136c5b5
Varex Silver <limposfeed@gmail.com>
2023-03-20 17:28:19 +0200

    Remove typedoc, fix links

--- typedoc.js
+++
@@ -1,5 +0,0 @@
-// typedoc.js
-module.exports = {
-	entryPoints: ["packages/typechain-polkadot/**/*.ts", "packages/typechain-polkadot-parser/**/*.ts"],
-	exclude: ["out/**/*.ts", "artifacts/**/*.ts", "tests/**/*.ts"],
-};
